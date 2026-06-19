1e84c920621b9c4950439bac8f45cc94a7a18aae
bergh026 <herman.berghuijs@wur.nl>
2026-06-19 16:10:14 +0200

    LINTUL Cassava NPK simulations were succesfull run using both a Python file (example.py) and a Jupyter Notebook (example.ipynb)

--- example/example.py
+++ example/example.py
@@ -1,8 +1,9 @@
+from matplotlib import pyplot as plt
 from pcse.base import ParameterProvider
 from pcse.engine import Engine
 from pcse.input import CABOWeatherDataProvider
-from pathlib import Path
 import input_file_paths as ifp
+import output_file_paths as ofp
 import pandas as pd
 import yaml
 
@@ -14,9 +15,6 @@ def run_model_and_collect_output(agrod, parameters, wdp, modelconf_fp):
     return df_out
 
 def main():
-    weather_fn = "nig2.016"
-    station_id = 2
-
     with open(ifp.crop_fp) as f:
         cropd = yaml.safe_load(f.read())
     with open(ifp.agro_fp) as f:
@@ -25,15 +23,28 @@ def main():
         soild = yaml.safe_load(f.read())
     with open(ifp.site_fp) as f:
         sited = yaml.safe_load(f.read())
-    wdp = CABOWeatherDataProvider(fname="nig2", fpath=ifp.weather_dir)
+    wdp = CABOWeatherDataProvider(fname=ifp.weather_fn, fpath=ifp.weather_dir)
     parameters = ParameterProvider(cropdata=cropd,
                                    sitedata=sited,
                                    soildata=soild)
-    df_out_wlp = run_model_and_collect_output(agrod, parameters, wdp, ifp.conf_fp_wlp)
+
     df_out_pp = run_model_and_collect_output(agrod, parameters, wdp, ifp.conf_fp_pp)
+    df_out_wlp = run_model_and_collect_output(agrod, parameters, wdp, ifp.conf_fp_wlp)
     df_out_wnlp = run_model_and_collect_output(agrod, parameters, wdp, ifp.conf_fp_wnlp)
 
+    df_out_pp.to_excel(ofp.output_pp_fp)
+    df_out_wlp.to_excel(ofp.output_wlp_fp)
+    df_out_wnlp.to_excel(ofp.output_wnlp_fp)
 
+    fig, ax = plt.subplots()
+    ax.set_xlabel("Date (YYYY-MM-DD)")
+    ax.set_ylabel("Yield (g DM m$^{-2}$ ground)")
+    ax.plot(df_out_pp.day, df_out_pp.WSO, linestyle="-", color = 'g', label="Potential production")
+    ax.plot(df_out_wlp.day, df_out_wlp.WSO, linestyle="-", color = 'b', label="Water-limited production")
+    ax.plot(df_out_wnlp.day, df_out_wnlp.WSO, linestyle="-", color = 'r', label="Water-and-nutrient limited production")
+    ax.legend()
+    fig.autofmt_xdate()
+    fig.savefig(ofp.fig_fp, dpi = 900)
 
 if __name__ == "__main__":
     main()
